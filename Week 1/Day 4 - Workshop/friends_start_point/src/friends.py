eb8e6b34e8dc7236b6daeb54c5f1ce3e65fa4cb2
juanMoleroD <juanldimeco@gmail.com>
2022-07-07 16:35:29 +0100

    passed test 3

--- Week 1/Day 4 - Workshop/friends_start_point/src/friends.py
+++ Week 1/Day 4 - Workshop/friends_start_point/src/friends.py
@@ -3,3 +3,7 @@ def get_name(person):
 
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    return food in person["favourites"]["snacks"]
+

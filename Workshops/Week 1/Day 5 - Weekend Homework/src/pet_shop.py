8ba27697e4ce4cd2d6ae7596ca2725f8928abd0a
juanMoleroD <juanldimeco@gmail.com>
2022-07-08 11:33:31 +0100

    passed test 5

--- Workshops/Week 1/Day 5 - Weekend Homework/src/pet_shop.py
+++ Workshops/Week 1/Day 5 - Weekend Homework/src/pet_shop.py
@@ -7,3 +7,6 @@ def get_total_cash(pet_shop):
 
 def add_or_remove_cash(pet_shop, amount):
     pet_shop["admin"]["total_cash"] += amount
+
+def get_pets_sold(pet_store):
+    return pet_store["admin"]["pets_sold"]
